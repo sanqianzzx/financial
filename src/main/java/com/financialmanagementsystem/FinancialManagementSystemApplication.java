473ba0c350902604d925e14673fa497c0252f069
三千世界 <1518353126@qq.com>
2023-12-14 21:30:41 +0800

    第一次提交

---
+++ src/main/java/com/financialmanagementsystem/FinancialManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.financialmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/financialmanagementsystem/FinancialManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
